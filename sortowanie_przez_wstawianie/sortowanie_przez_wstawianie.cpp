9ef6e65efcbb1a9376c74874e4f09c193c6a652c
pirtaman <150184568+pirtaman@users.noreply.github.com>
2023-11-07 21:06:50 +0100

    Update sortowanie_przez_wstawianie.cpp

--- sortowanie_przez_wstawianie/sortowanie_przez_wstawianie.cpp
+++ sortowanie_przez_wstawianie/sortowanie_przez_wstawianie.cpp
@@ -1,29 +1,24 @@
-﻿#include <iostream>
+
+#include <iostream>
 using namespace std;
 
 int n;
-int tab[1000000];
+int do_posortowania[1000000];
 
 int main()
 {
     cin >> n;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n;i++) cin >> do_posortowania[i];
+    for (int i = 1; i<n;i++)
     {
-        cin >> tab[i];
-    }
-    for (int i = 1; i < n; i++)
-    {
-        int temp = tab[i];
+        int sprawdzana_liczba = do_posortowania[i];
         int j = i - 1;
-        while (j >= 0 && tab[j] > temp)
-        {
-            tab[j + 1] = tab[j];
+        while (j >= 0 && do_posortowania[j] > sprawdzana_liczba){
+            do_posortowania[j + 1] = do_posortowania[j];
             j--;
         }
-        tab[j + 1] = temp;
+        do_posortowania[j + 1] = sprawdzana_liczba;
     }
-    for (int i = 0; i < n; i++)
-    {
-        cout << tab[i];
-    }
+    for (int i = 0; i < n; i++) cout << do_posortowania[i];
+    
 }
